71883d2944e298e13b2bb72f33d9c2c127b3dc5e
Jianning Li <ljn16@mails.tsinghua.edu.cn>
2020-01-23 16:33:06 +0100

    Add files via upload

--- ttest.py
+++ ttest.py
@@ -2,7 +2,7 @@ from scipy.stats import ttest_ind
 import numpy as np 
 
 # calculate the significance
-value, pvalue = ttest_ind(mean_o_implant, mean_p_implant, equal_var=True)
+value, pvalue = ttest_ind(teamA_score, teamB_score, equal_var=False)
 print(value, pvalue)
 if pvalue > 0.05:
 	print('Samples are likely drawn from the same distributions (fail to reject H0)')
